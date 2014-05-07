1de337bc7b1c0e83669cc80fc96bf5dddfb26d68
System Administrator <samer.asbah@gmail.com>
2014-05-07 16:31:48 +0300

    add rasha project

---
+++ expressionevaluator/expressionevaluator/Evaluator.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace expressionevaluator
+{
+    class Evaluator
+    {
+  		// Create Stack Object to save the Expression
+        private Stack stk = new Stack();
+        
+       /**
+    	* Evaluate
+	    * @param: string exp
+	    * description: this method goes over the expression char by char
+	    * and push characters to stack till it finds a close Parentheses ')'
+	    * after that we pop the char from stack till we find the open Parentheses ')' in stack.
+	    * We run the operation for elements found in pop result and then push it back to stack 
+	    * in case we have more than one operation in an expression
+     	*/
+
+        public void Evaluate(string exp)
+        {
+
+            try
+            {
+				
+                string Digit = "";
+				
+				// For loop char in the expression
+                foreach (char element in exp)
+                {
+
+					
+                    if (Char.IsDigit(element))
+                    {
+                        Digit = Digit + element.ToString();
+                    }
+                    else
+                    {
+                        if (element != ')')
+                        {
+							// This is added to solve numbers with more than one digit
+                            if (Digit != "")
+                            {
+                                stk.Push(Digit);
+                                Digit = "";
+                            }
+
+                            stk.Push(element.ToString());
+
+                        }
+                        else
+                        {
+
+                            if (Digit != "")
+                            {
+                                stk.Push(Digit);
+                                Digit = "";
+                            }
+
+                            decimal Num2 = decimal.Parse(stk.Pop().ToString());
+                            string Op = (string)stk.Pop();
+                            decimal Num1 = decimal.Parse(stk.Pop().ToString());
+                            string out1 = (string)stk.Pop();
+
+                            switch (Op)
+                            {
+                                case "+":
+                                    stk.Push((Num1 + Num2).ToString());
+                                    break;
+                                case "-":
+                                    stk.Push((Num1 - Num2).ToString());
+                                    break;
+                                case "*":
+                                    stk.Push((Num1 * Num2).ToString());
+                                    break;
+                                case "/":
+                                    stk.Push((Num1 / Num2).ToString());
+                                    break;
+                                case "%":
+                                    stk.Push((Num1 % Num2).ToString());
+                                    break;
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                string lastValue;
+                lastValue = (string)stk.Pop();
+
+                if (lastValue.All(Char.IsDigit))
+                    Console.WriteLine(lastValue);
+                else
+                    Console.WriteLine("Can’t parse the expression");
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("Can’t parse the expression.");
+            }
+
+        }
+
+    }
+}
---
+++ expressionevaluator/expressionevaluator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace expressionevaluator
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+        	// Output Message For User to Enter Expression
+            Console.WriteLine("Please Enter Expression:");
+            
+            string str;
+            // Read Input From User
+            str = Console.ReadLine();
+            
+            // While Input From User Not Equal Quit Run Expression, Otherwise quit the Console Application
+            while (str != "quit")
+            {
+	            // Create Object Evaluator
+                Evaluator ev = new Evaluator();
+                
+                // Pass Express to object to be evaluated and to show result output 
+                // Each mathematical operation in the expression should be between a Parentheses
+                // Example (((1+6) – 2) / 2 )
+                ev.Evaluate(str);
+                
+                // ReEnter a new Expression
+                Console.WriteLine("Please Enter Expression Ex. (a+b) :");
+                
+                str = Console.ReadLine();
+            }
+        }
+
+    }
+
+}
